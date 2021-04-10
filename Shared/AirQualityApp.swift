99f2ffea9016194fbecd7e12d9a4d595016e4223
George Kapoya <george.kapoya@fi.abb.com>
2021-04-10 10:59:21 +0300

    first commit

---
+++ Shared/AirQualityApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirQualityApp.swift
+//  Shared
+//
+//  Created by George Kapoya on 10.4.2021.
+//
+
+import SwiftUI
+
+@main
+struct AirQualityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
